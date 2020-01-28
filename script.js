4fc40972310b40f01848d4c21ad32853b30d9478
courtlandmessam <messamcourtland@gmail.com>
2020-01-28 09:12:39 -0500

    added js logic, html divs and and first iteration of css styling on the fields on my form

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const error = document.getElementById('error-message');
-const email = document.getElementById('eadress');
 const form = document.getElementById('form');
+const error = document.getElementById('error-message');
+const fname = document.getElementById('fname');
+const fnameError = document.getElementById('fname-error');
+const lname = document.getElementById('lname');
+const lnameError = document.getElementById('lname-error');
+const email = document.getElementById('eaddress');
+const emailError = document.getElementById('eaddress-error');
+const pword = document.getElementById('pword');
+const pwordError = document.getElementById('pword-error');
+
+
 
 form.addEventListener('submit', (e) => {
-  if (!validateEmail(email.value)) {
+  if (fname.value == "") {
     e.preventDefault();
-    error.innerHTML = "This is not a valid email";
+    fnameError.innerHTML = "First name cant be empty";
   }
+
+  if (lname.value == "") {
+    e.preventDefault();
+    lnameError.innerHTML = "Last name cant be empty";
+  }
+
+  if (email.value == "") {
+    e.preventDefault();
+    emailError.innerHTML = "Email cant be empty";
+  } else if (!validateEmail(email.value)) {
+      e.preventDefault();
+      emailError.innerHTML = "This is not a valid email";
+    }
+
+  if (pword.value == "") {
+    e.preventDefault();
+    pwordError.innerHTML = "Password cant be empty";
+  }
+
 
 })
 
