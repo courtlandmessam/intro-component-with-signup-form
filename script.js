dbdb611d10e8d23a53928a7f8e21df53fa8cead6
courtlandmessam <messamcourtland@gmail.com>
2020-01-28 06:58:16 -0500

    added error message functionality to form to detect if a user inputs a valid email

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const error = document.getElementById('error-message');
 const email = document.getElementById('eadress');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  if (!validateEmail(email.value)) {
+    e.preventDefault();
+    error.innerHTML = "This is not a valid email";
+  }
+
+})
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
